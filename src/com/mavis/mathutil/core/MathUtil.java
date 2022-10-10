168fcb76906b33bf0ac45fb681c1e608aa86ef4c
tranlebaongoc1512 <tranlebaongoc1512@gmail.com>
2022-10-10 16:36:44 +0700

    apply getF() to recursive method

--- src/com/mavis/mathutil/core/MathUtil.java
+++ src/com/mavis/mathutil/core/MathUtil.java
@@ -17,7 +17,33 @@ public class MathUtil {
     //vi giai thừa tăng cực nhanh, 211 tràn kiểu long (18 số 0)
     //ko có giai thừa âm
     //quy ước: n nhận vào từ 0...20!
-    public static long getFactorial(int n) {
+    //viết tính giai thừa theo đệ quy
+    //n! = 1.2.3.4...n
+    //c1: viết kiểu for, con heo đất, nhồi dần kết quả vào 1 biến
+    //product = product * i; //i chạy từ 1...n
+    //c2: đệ quy - recursion
+    //GỌI LẠI CHỈNH MÌNH VỚI 1 QUY MÔ/GIÁ TRỊ KHÁC
+    //Búp bê của người Nga: con búp bê bự chứa con búp bê nhỏ bên trong
+    //                                         con trong chứa con khác... khoảng 12 con
+    //                                         lặp lại chính mình 12 lần
+    //5! = ??? 1.2.3.4.5
+    //5! =           4! . 5 = 5 x 4!
+    //4! = 4 x 3!
+    //3! = 3 x 2!
+    //2! = 2 x 1!
+    //1! = 1
+    //n! = n x (n - 1)!
+    public static long getFactorial(int n){
+        if (n < 0 || n > 20)
+            throw new IllegalArgumentException("Invalid argument. n must be between 0 to 20");
+        if (n == 0 || n == 1)
+            return 1;
+        //sống sót đến đây n = 2...20
+        //hàm này đang tính n! mà n! = n * (n - 1)!
+        return n * getFactorial(n - 1);
+        //n! = n * (n - 1)!
+    }
+/*    public static long getFactorial(int n) {
         if (n < 0 || n > 20)
             throw new IllegalArgumentException("Invalid argument. n must be between 0 to 20");
         if (n == 0 || n == 1)
@@ -27,7 +53,7 @@ public class MathUtil {
             product *= i; //product = product * i
         return product;
 
-    }
+    }*/
     //tư duy viết code theo kiểu gọi là TDD - Test Driven Development
     //Test First Development: khi viết code thì song song đó phải viết các tính huống kiểm thử code/class/kiểm thử app
     //tư duy viết code kiểm thử:
